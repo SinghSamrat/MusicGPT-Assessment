f1b250c13dc67964b85232a77aaf4efe8dd0aa6c
SinghSamrat <samratsingh@Samrats-MacBook-Air.local>
2026-03-01 19:10:24 +0545

    Initial Project Configuration

---
+++ MusicGPT-Assessment/MusicGPT-Assessment/MusicGPT_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicGPT_AssessmentApp.swift
+//  MusicGPT-Assessment
+//
+//  Created by Samrat Singh on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicGPT_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
