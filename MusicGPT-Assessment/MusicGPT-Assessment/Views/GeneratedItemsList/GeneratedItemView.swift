9de024ad0b26b0308f3a54c8b2502751d4343301
SinghSamrat <samratsingh@Samrats-MacBook-Air.local>
2026-03-02 14:00:22 +0545

    created view for generated-items-list

---
+++ MusicGPT-Assessment/MusicGPT-Assessment/Views/GeneratedItemsList/GeneratedItemView.swift
@@ -0,0 +1,45 @@
+//
+//  GeneratedItemView.swift
+//  MusicGPT-Assessment
+//
+//  Created by Samrat Singh on 02/03/2026.
+//
+
+
+import SwiftUI
+
+struct GeneratedItemView: View  {
+    var generatedItem: GeneratedItem
+    var itemTapped:() -> Void
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(generatedItem.artworkName)
+                .foregroundColor(.white)
+                .frame(width: 64, height: 64)
+                .cornerRadius(16.0)
+            
+            VStack(alignment: .leading) {
+                Text(generatedItem.title)
+                    .font(.system(size: 16))
+                Text(generatedItem.originalPrompt)
+                    .font(.system(size: 14))
+                    .foregroundStyle(.secondary)
+                    .lineLimit(1)
+            }
+            
+            Spacer()
+            
+            Button {
+                itemTapped()
+            } label: {
+                Image(systemName: "ellipsis")
+                    .tint(.secondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    GeneratedItemView(generatedItem: sampleGeneratedItem) {}
+}
---
+++ MusicGPT-Assessment/MusicGPT-Assessment/Views/GeneratedItemsList/GeneratedItemsListView.swift
@@ -0,0 +1,83 @@
+//
+//  AIGenerateScreenView.swift
+//  MusicGPT-Assessment
+//
+//  Created by Samrat Singh on 02/03/2026.
+//
+
+import SwiftUI
+
+struct GeneratedItemsListView: View {
+    @State var viewModel = GeneratedItemsListViewModel()
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 16) {
+            HStack(spacing: 6) {
+                TitleView()
+                Spacer()
+            }
+            .frame(height: 35)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(viewModel.allItems, id: \.self) { item in
+                        GeneratedItemView(generatedItem: item) {}
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            CreateButtonView() {
+                // create logic
+            }
+        }
+        .padding(.leading)
+    }
+}
+
+
+struct CreateButtonView: View {
+    var createTapped: () -> Void
+    
+    var body: some View {
+        Button {
+            createTapped()
+        } label: {
+            ZStack {
+                Rectangle()
+                    .frame(width: 103, height: 44)
+                    .cornerRadius(100)
+                    .foregroundColor(.white.opacity(0.1))
+                
+                HStack(spacing: 4) {
+                    Image("stars")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20)
+                    
+                    Text("Create")
+                        .font(.system(size: 14, weight: .bold))
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+
+struct TitleView: View {
+    var body: some View {
+        Image("Subtract")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        Text("MusicGPT")
+            .font(.system(size: 18))
+            .fontWeight(.medium)
+    }
+}
+
+
+#Preview {
+    GeneratedItemsListView()
+}
